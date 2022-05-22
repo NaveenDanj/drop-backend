bdd2196ce38610bd96c0ab5eb5d1cef819445b26
Naveen Dhananjaya <naveenhettiwaththa@gmail.com>
2022-05-22 13:10:39 +0530

    added add release note endpoint and get all release note endpoint

--- app/Models/ReleaseLog.php
+++ app/Models/ReleaseLog.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class ReleaseLog extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'release_date',
+        'version',
+        'release_log'
+    ];
 }
