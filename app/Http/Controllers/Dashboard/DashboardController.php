108b594adfffe87c15d1eda435d9e91d226be4a7
Naveen Dhananjaya <naveenhettiwaththa@gmail.com>
2022-05-24 00:37:49 +0530

    added endpoint to retrive user files

---
+++ app/Http/Controllers/Dashboard/DashboardController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers\Dashboard;
+
+use App\Http\Controllers\Controller;
+use App\Models\UserFile;
+use Illuminate\Http\Request;
+
+class DashboardController extends Controller
+{
+
+    public function userFiles(Request $request , $userid){
+
+        //get all user files and paginate
+        $userFiles = UserFile::where('user_id' , $userid)->paginate(10);
+
+        // get logged in user
+        $user = auth()->user();
+
+        if($user->id != $userid){
+            return response()->json([
+                'error' => 'Unauthorized'
+            ] , 401);
+        }
+
+        return response()->json([
+            'userFiles' => $userFiles,
+        ]);
+
+    }
+
+}
