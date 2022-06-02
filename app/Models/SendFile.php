3d1acddbb8b8fdd6e9bf5c49c0abb567f695d136
Naveen Dhananjaya <naveenhettiwaththa@gmail.com>
2022-06-02 22:01:10 +0530

    create the SendFile model

--- app/Models/SendFile.php
+++ app/Models/SendFile.php
@@ -8,4 +8,29 @@ use Illuminate\Database\Eloquent\Model;
 class SendFile extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'from_user',
+        'to_user',
+        'to_email',
+        'file_id',
+        'subject',
+        'message'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo('App\Models\User');
+    }
+
+    public function file()
+    {
+        return $this->belongsTo('App\Models\UserFile');
+    }
+
+    public function getFromUser()
+    {
+        return $this->belongsTo('App\Models\User', 'from_user');
+    }
+
 }
